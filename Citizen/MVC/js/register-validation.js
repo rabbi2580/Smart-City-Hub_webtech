3506daef9ec866aa5b27fab46deb72b7b00d0354
rabbi2580 <129550772+rabbi2580@users.noreply.github.com>
2026-01-04 22:30:22 +0600

    calling the element by using get element

---
+++ Citizen/MVC/js/register-validation.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded",function(){
+    const form =document.getElementById('registerForm');
+    const password= document.getElementById("password");
+    const confirmPassword =document.getElementById('confirm_password');
+    form.
+})
