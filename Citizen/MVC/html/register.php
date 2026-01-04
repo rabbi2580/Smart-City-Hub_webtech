89fe631620ec4a261dd4715da5fc7df5efe5076e
rabbi2580 <129550772+rabbi2580@users.noreply.github.com>
2026-01-04 20:57:13 +0600

    form creation done

--- Citizen/MVC/html/register.php
+++ Citizen/MVC/html/register.php
@@ -13,7 +13,27 @@
     <?php if($message):?>
         <p class="message"><?php echo $message;?> </p>
     <?php endif;?>
+    <form method="POST" action="SmartCityHub/Citizen/MVC/php/register_controller.php">
+        <label>First Name </label>
+        <input type="text" name="first_name" required>
+        <label>Last Name </label>
+        <input type="text" name="last_name" required>
+        <label>Username </label>
+        <input type="text" name="username" required>
+        <label>Password</label>
+        <input type="password" name="password" required>
+        <label>Confirm password </label>
+        <input type="password" name="confirm password" required>
+        <label>ID Number</label>
+        <input type="text" name="id_number" required>
+        <label>Phone Number</label>
+        <input type="text" name="phone" required>
+        <label>Location</label>
+        <input type="text" name="location" required>
+        <button type="submit"> Register </button>
     
+    </form>
+    <p>Already have an Smart City Hub account?<a href="login.php">Login here</a> </p>
     </div>
 </body>
 </html>
