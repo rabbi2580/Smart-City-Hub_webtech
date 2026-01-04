d9087a19a2bcf7d263f0bc7f5ba454f72b441b69
rabbi2580 <129550772+rabbi2580@users.noreply.github.com>
2026-01-04 20:38:12 +0600

    head part done

--- Citizen/MVC/html/register.php
+++ Citizen/MVC/html/register.php
@@ -3,6 +3,9 @@
     <head>
         <meta charset="UTF-8">
         <meta name="viewpoint" content="width=device-width, initial-scale=1.0">
-        
+        <title> Citizen Registration </title>
+        <link rel="stylesheet" href="SmartCityHub/Citizen/MVC/css/citizen-style.css">
+
     </head>
+
 </html>
