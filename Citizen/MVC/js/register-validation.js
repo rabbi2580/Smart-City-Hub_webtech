a42a75864609c800364ba82cb4096239e75d37d3
rabbi2580 <129550772+rabbi2580@users.noreply.github.com>
2026-01-04 22:37:14 +0600

    phone number validation

--- Citizen/MVC/js/register-validation.js
+++ Citizen/MVC/js/register-validation.js
@@ -2,5 +2,23 @@ document.addEventListener("DOMContentLoaded",function(){
     const form =document.getElementById('registerForm');
     const password= document.getElementById("password");
     const confirmPassword =document.getElementById('confirm_password');
-    form.
+    form.addEventListener('submit',function(a){
+        if(password.value!==confirmPassword.value){
+            e.preventDefault();
+            alert("Password not matched");
+            return;
+        }
+        if(password.value.length<8){
+            e.preventDefault();
+            alert('Password must be at least 8 char')
+            return;
+        }
+        const phone=document.querySelector('input[name="phone"]');
+        if(!/^\d{11}$/.test(phone.value)){
+            e.preventDefault();
+            alert("Phone number must be 11 digit")
+            return;
+        }
+
+    });
 })
