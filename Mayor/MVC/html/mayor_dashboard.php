d975bcf24b43579f234b19fe5ef6a33815b01653
rabbi2580 <129550772+rabbi2580@users.noreply.github.com>
2026-01-04 23:57:05 +0600

    mayor

---
+++ Mayor/MVC/html/mayor_dashboard.php
@@ -0,0 +1,8 @@
+<!DOCTYPE html>
+<html lang="en">
+<head> 
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mayor Dashboard </title>
+    <link rel="stylesheet" href="../css/mayor-style.css">
+</head>
+</html>
