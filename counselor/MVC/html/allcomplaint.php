aecc253069b12e8771cb5c8680c15700b050ece2
Niharika Ibrahim <niharika8ibrahim@gmail.com>
2026-01-08 22:11:41 +0600

    allcomplaint dashboard first

---
+++ counselor/MVC/html/allcomplaint.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+<title>All Complaints | Counselor Dashboard</title>
+</head>
+
+<body>
+
+<header>
+  <h2>Smart City Hub – Counselor Panel</h2>
+  <div>All Complaints</div>
+</header>
