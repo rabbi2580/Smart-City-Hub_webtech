1dca7279ff362b5e4ac93e91114947ffe77aea79
Niharika Ibrahim <niharika8ibrahim@gmail.com>
2026-01-08 22:13:16 +0600

    html for all complaint dashboard

--- counselor/MVC/html/allcomplaint.php
+++ counselor/MVC/html/allcomplaint.php
@@ -11,3 +11,28 @@
   <h2>Smart City Hub – Counselor Panel</h2>
   <div>All Complaints</div>
 </header>
+<div class="container">
+  <table>
+    <tr>
+      <th>ID</th>
+      <th>Description</th>
+    </tr>
+
+    <tr>
+      <td>101</td>
+      <td>Broken street light near park</td>
+    </tr>
+
+    <tr>
+      <td>102</td>
+      <td>Garbage not collected in the area</td>
+    </tr>
+
+    <tr>
+      <td>103</td>
+      <td>Pothole on main road</td>
+    </tr>
+  </table>
+</div>
+</body>
+</html>
