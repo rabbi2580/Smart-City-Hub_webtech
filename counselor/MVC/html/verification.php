e15333c5bb963f4aefb68152caf7e7a5d54b6e85
Niharika Ibrahim <niharika8ibrahim@gmail.com>
2026-01-08 22:21:11 +0600

    html for verification dashboard

---
+++ counselor/MVC/html/verification.php
@@ -0,0 +1,37 @@
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<title>Pending Verification</title>
+
+<body>
+<header>
+  <h2>Pending Verification Complaints</h2>
+</header>
+<div class="container">
+ <table>
+    <tr>
+      <th>ID</th>
+          <th>Description</th>
+    </tr>
+
+    <tr>
+      <td>201</td>
+      <td>Water leakage on main road</td>
+    </tr>
+
+    <tr>
+      <td>202</td>
+      <td>Street light not working</td>
+    </tr>
+
+    <tr>
+      <td>203</td>
+      <td>Garbage pile near school</td>
+    </tr>
+  </table>
+</div>
+
+</body>
+</html>
