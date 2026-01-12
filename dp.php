838ede7b2434668c701343e0d631cc621376928c
rabbi2580 <129550772+rabbi2580@users.noreply.github.com>
2026-01-13 00:37:03 +0600

    full connection code

--- dp.php
+++ dp.php
@@ -1,2 +1,11 @@
 <?php
 $host="localhost";
+$user="root";
+$pass="";
+$dbname="smart_city_hub";
+$conn=mysqli_connect($host,$user,$pass,$dbname);
+if(!$conn){
+    die("Connection is not created,connection failed:". mysqli_connect_error());
+
+}
+?>
