46fade4d4b76641c4eda8a5c6898d90163871bab
rabbi2580 <129550772+rabbi2580@users.noreply.github.com>
2026-01-14 18:23:45 +0600

    updated view file

--- Mayor/MVC/html/mayor_dashboard.php
+++ Mayor/MVC/html/mayor_dashboard.php
@@ -1,6 +1,7 @@
 <!DOCTYPE html>
 <html lang="en">
 <head> 
+    <meta charset="URF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Mayor Dashboard </title>
     <link rel="stylesheet" href="../css/mayor-style.css">
@@ -9,14 +10,14 @@
     <div class ="container">
         <div class ="header">
             <h1>Welcome,Mayor</h1>
-            <a href="change_information.php" class="change_info_button">Change Information</a>
+            <a href="change_information.php" class="change_info_button">Change Profile Information</a>
         </div>
         <p>This is your Smart City Hub control panel</p> 
         
         <div class="menu">
             <a href="view_all_complaints.php" class="btn">View All Complaints</a>
             <a href="view_statistics.php" class="btn">View Statistics </a>
-            <a href="send_rewards.php" class="btn">Send Rewards</a>
+           
             <a href="final_approvals.php" class="btn"> Final Approvals </a>
 
         </div>
