98d28bdc5e91eb31402248d872540e16491c660d
rabbi2580 <129550772+rabbi2580@users.noreply.github.com>
2026-01-14 18:35:15 +0600

    code for view all complains controller

--- Mayor/MVC/php/view_all_complaints_controller.php
+++ Mayor/MVC/php/view_all_complaints_controller.php
@@ -10,3 +10,10 @@ $sql="SELECT c.id,c.title,c.type,c.status,c.location,u.name AS citizen_name
       FROM complaints c
       LEFT JOIN users u ON c.citizen_id=u.id
       ORDER BY c.submitted_at DESC";
+$result =mysqli_query($conn,$sql);
+$complaints=[];
+while($row=mysqli_fetch_array($result)){
+    $complaints[]=$row;
+}
+include '../html/view_all_complaints.php';
+?>
