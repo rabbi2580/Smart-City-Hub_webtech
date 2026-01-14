b846b48470982582ef04c32ed521adabdb19a48b
rabbi2580 <129550772+rabbi2580@users.noreply.github.com>
2026-01-14 18:28:17 +0600

    if role is not matched then take to login page code

---
+++ Mayor/MVC/php/view_all_complaints_controller.php
@@ -0,0 +1,7 @@
+<?php
+session_start();
+include'../../../db.php';
+if(!isset($_SESSION['user_id'])||$_SESSION['role']!=='mayor'){
+    header('Location: ../../../Citizen/MVC/html/login.php');
+    exit;
+}
