9abfa4b4accc6517584ee1a61e755513ec45198e
rabbi2580 <129550772+rabbi2580@users.noreply.github.com>
2026-01-14 19:06:10 +0600

    correction

--- Mayor/MVC/php/view_statistics_controller.php
+++ Mayor/MVC/php/view_statistics_controller.php
@@ -5,8 +5,10 @@ if(!isset($_SESSION['user_id'])||$_SESSION['role']!=='mayor'){
     header('Location: ../../../Citizen/MVC/html?login.php');
     exit;
 }
-$total = mysqli_fetch_row($conn, "SELECT COUNT(*) FROM complaints"))[0];
+$total = mysqli_fetch_row(mysqli_query($conn, "SELECT COUNT(*) FROM complaints"))[0];
 $completed = mysqli_fetch_row(mysqli_query($conn, "SELECT COUNT(*) FROM complaints WHERE status IN ('completed', 'final_approved')"))[0];
 $pending = mysqli_fetch_row(mysqli_query($conn, "SELECT COUNT(*) FROM complaints WHERE status IN ('submitted', 'valid', 'forwarded')"))[0];
 $rejected = mysqli_fetch_row(mysqli_query($conn, "SELECT COUNT(*) FROM complaints WHERE status = 'rejected'"))[0];
 
+include '../html/view_statistics.php';
+?>
