c2b775342af69f5b1c96b5961eb3bc2e82dc81c7
rabbi2580 <129550772+rabbi2580@users.noreply.github.com>
2026-01-15 18:40:18 +0600

    code for approve and rejected button

--- Mayor/MVC/php/final_approve_ajax.php
+++ Mayor/MVC/php/final_approve_ajax.php
@@ -5,3 +5,18 @@ if(!isset($_SESSION['user_id'])||$_SESSION['role']!=='mayor'){
     echo "Unauthorized access!";
     exit;
 }
+if($_SERVER['REQUEST_METHOD']=='POST'){
+    $action =$_POST['action'];
+    $selected=$_POST['selected']??[];
+    if(empty($selected)){
+        echo "No complain selected";
+        exit;
+    }
+    $new_status=($action=='approve')?'final_approved': 'rejected';
+    $ids= implode(',',array_map('intval',$selected));
+    $sql="UPDATE complaints SET status ='$new_status',mayor_id={$_SESSION['user_id']} WHERE id IN ($ids)";
+    mysqli_query($conn,$sql);
+    echo($action=='approve')? "selected complaints approved successfully" :"slected complaint rejected";
+
+}
+?>
