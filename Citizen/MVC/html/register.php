b77919d618bf481e829b1d2bf8f3763db675194c
rabbi2580 <129550772+rabbi2580@users.noreply.github.com>
2026-01-15 22:21:39 +0600

    updated code for the register view page

--- Citizen/MVC/html/register.php
+++ Citizen/MVC/html/register.php
@@ -11,26 +11,26 @@
 <body>
     <div class="container">
         <h2>Citizen Registration </h2>
-    <?php if(isset($message)):?>
-        <p class="message"><?php echo $message;?> </p>
-    <?php endif;?>
-    <form id="registerForm" method="POST" action="../php/register_controller.php">
+        <div id="message" style="padding: 15px; margin: 20px 0; border-radius: 6px;">
+            <?php echo $message; ?>
+        </div>
+        <form method="POST" action="../php/register_controller.php"> 
         <label>First Name </label>
-        <input type="text" name="first_name" required>
+        <input type="text" name="first_name" value="<?php echo htmlspecialchars($form_data['first_name']??''); ?>" required><br>
         <label>Last Name </label>
-        <input type="text" name="last_name" required>
+        <input type="text" name="last_name" value="<?php echo htmlspecialchars($form_data['last_name']??''); ?>" required><br>
         <label>Username </label>
-        <input type="text" name="username" required>
+        <input type="text" name="username" value="<?php echo htmlspecialchars($form_data['username'] ?? ''); ?>" required><br><br>
         <label>Password</label>
-        <input type="password" name="password" required>
+        <input type="password" name="password" required><br>
         <label>Confirm password </label>
-        <input type="password" name="confirm_password" required>
+        <input type="password" name="confirm_password" required><br>
         <label>ID Number</label>
-        <input type="text" name="id_number" required>
+        <input type="text" name="id_number" value="<?php echo htmlspecialchars($form_data['id_number']??''); ?>" required><br>
         <label>Phone Number</label>
-        <input type="text" name="phone" required>
+        <input type="text" name="phone" value="<?php echo htmlspecialchars($form_data['phone']??''); ?>" required><br>
         <label>Location</label>
-        <input type="text" name="location" required>
+        <input type="text" name="id_number" value="<?php echo htmlspecialchars($form_data['location']??''); ?>" required><br>
         <button type="submit"> Register </button>
     
     </form>
