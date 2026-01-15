65a03eac24f5e1cbaed25b08272003519617cc06
rabbi2580 <129550772+rabbi2580@users.noreply.github.com>
2026-01-15 19:54:08 +0600

    code for role mayor

--- Mayor/MVC/php/send_rewards_controller.php
+++ Mayor/MVC/php/send_rewards_controller.php
@@ -1,4 +1,7 @@
 <?php
 session_start();
 include '../../../db.php';
-if
+if (!isset($_SESSION['user_id'])||$_SESSION['role']!=='mayor'){
+    header('Location: ../../../Citizen/MVC/html/login.php');
+    exit;
+}
