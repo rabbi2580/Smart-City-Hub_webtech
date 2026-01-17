2f724608caf8f1ee6c0f998fe57516029b257225
mahfuzaMou71 <mahfuzamou562@gmail.com>
2026-01-17 10:49:50 +0600

    updated logout.php

--- Citizen/MVC/php/logout.php
+++ Citizen/MVC/php/logout.php
@@ -1,5 +1,17 @@
 <?php
 session_start();
+require_once __DIR__ . "/../db/db.php";
+global $conn;
+
+if (isset($_COOKIE["remember_token"]) && $_COOKIE["remember_token"] !== "") {
+    $tokenHash = hash("sha256", $_COOKIE["remember_token"]);
+    $stmt = $conn->prepare("DELETE FROM remember_tokens WHERE token_hash = ?");
+    $stmt->bind_param("s", $tokenHash);
+    $stmt->execute();
+}
+
+setcookie("remember_token", "", time() - 3600, "/");
+
 session_unset();
 session_destroy();
 
