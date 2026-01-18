0c2d3759d88d609937def8024faa93b20fc6f934
mahfuzaMou71 <mahfuzamou562@gmail.com>
2026-01-18 18:38:24 +0600

    fixed some minor issues

--- Citizen/MVC/html/dashboard.php
+++ Citizen/MVC/html/dashboard.php
@@ -4,7 +4,7 @@ require_once __DIR__ . "/../php/auth.php";
 <!DOCTYPE html>
 <html>
 <head>
-    
+    <meta charset="UTF-8">
     <title>Citizen Dashboard</title>
     <link rel="stylesheet" href="../css/style.css">
 </head>
